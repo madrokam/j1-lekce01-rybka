c25fb14b8a56f3fce39d9f41a8f20b27a6bed0ab
XMADK006 <xmadk006@studenti.czu.cz>
2023-02-07 20:09:05 +0100

    Zmen smer

--- src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
+++ src/main/java/cz/czechitas/java/lekce01/RybkaApplication.java
@@ -30,6 +30,13 @@ public class RybkaApplication extends CzechitasGameApplication {
     protected void onStart() {
         kamen = new Kamen(200, 200);
         rybka = new Rybka(100, 100);
+
+        rybka.plavatVpravo();
+        rybka.plavatVpravo();
+        rybka.plavatVpravo();
+        rybka.plavatVpravo();
+        rybka.plavatDolu();
+
     }
 
     public static void main(String[] args) {
